761d255d4b8ce90bf0e5c66e3d1ab4dcc4a46324
Jorgitor19 <91979954+Jorgitor19@users.noreply.github.com>
2021-11-15 17:42:13 +0100

    Update index.php

--- index.php
+++ index.php
@@ -30,8 +30,6 @@ print_r($meses);
 
 
 ?>
-
-
 <!DOCTYPE html>
 <html lang="es">
 
@@ -44,6 +42,13 @@ print_r($meses);
 <?php
     $muestra = [
             'nombre' => 'Jorge',
+            'apellido' => 'Fuentes',
+            'email' => 'jorgeylucas@gmail.com',
+            'telefono' => '662437243',
+            'postal' => '28400',
+            'ciudad' => 'Collado Villalba',
+            'domicilio' => 'C// Alfonso 13, Nº 151',
+            'contraseña' => 'Aaaaaaaa1#'
     ]
 ?>
 
@@ -64,25 +69,25 @@ print_r($meses);
             <input type="text" name="nombre" id="nombre" value="<?php echo $muestra['nombre'] ?>"><br><br>
 
             <label for="fapellido">Apellidos:</label>
-            <input type="text" name="apellido" id="apellido"><br><br>
+            <input type="text" name="apellido" id="apellido" value="<?php echo $muestra['apellido'] ?>" ><br><br>
 
             <label for="femail">Email:</label>
-            <input type="email" name="email"><br/><br/>
+            <input type="email" name="email" value="<?php echo $muestra['email'] ?>"><br/><br/>
 
             <label for="ftelefono">Telefono:</label>
-            <input id="telefono" name="telefono" type="tel" pattern="[0-9]{3}-[0-9]{3}-[0-9]{3}" required><br><br>
+            <input id="telefono" name="telefono" type="tel" value="<?php echo $muestra['telefono'] ?>"><br><br>
 
             <label for="fpostal">Código Postal:</label>
-            <input id="postal" name="postal" type="text" inputmode="numeric" pattern="^(?(^00000(|-0000))|(\d{5}(|-\d{4})))$"><br><br>
+            <input id="postal" name="postal" type="text" inputmode="numeric" value="<?php echo $muestra['postal'] ?>"><br><br>
 
             <label for="fciudad">Ciudad:</label>
-            <input id="ciudad" name="ciudad" type='country'><br><br>
+            <input id="ciudad" name="ciudad" type='country' value="<?php echo $muestra['ciudad'] ?>"><br><br>
 
-            <label for="fciudad">Domicilio:</label>
-            <input id="ciudad" name="ciudad" type='country'><br><br>
+            <label for="fdomicilio">Domicilio:</label>
+            <input id="domicilio" name="domicilio" type='country' value="<?php echo $muestra['domicilio'] ?>"><br><br>
 
             <label for="fcontraseña">Contraseña:</label>
-            <input type="text" name="contraseña" id="contraseña"><br /><br />
+            <input type="text" name="contraseña" id="contraseña"  value="<?php echo $muestra['contraseña'] ?>" ><br /><br />
 
             <input type="button" value="Confirmar" onClick="verificar()">
         </form>
