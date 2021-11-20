888a13b9ca9814e3e555a0d77a63657bead0f265
LucasFuentesGonzalez <91980274+LucasFuentesGonzalez@users.noreply.github.com>
2021-11-20 17:45:37 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,50 +1,76 @@
 <?php
 
-$meses = [];
-$string = file_get_contents("archivo.txt");
-$array = explode("\n",$string);
-foreach ($array as $fila){
-    $item = explode(" ",$fila);
-    $meses[] = [
-        'numero' => $item[0],
-        'nombre' => $item[0]
-    ];
-}
+    $meses = [];
+    $string = file_get_contents("archivo.txt");
+    $array = explode("\n",$string);
+    foreach ($array as $fila){
+        $item = explode(" ",$fila);
+        $meses[] = [
+            'numero' => $item[0],
+            'nombre' => $item[1]
+        ];
+    }
 
 
+    $nombre = filter_input(INPUT_GET, 'nombre', FILTER_SANITIZE_STRING);
+    $apellido = filter_input(INPUT_GET, 'apellido', FILTER_SANITIZE_STRING);
+    $email = filter_input(INPUT_GET, 'email', FILTER_SANITIZE_STRING);
+    $telefono = filter_input(INPUT_GET, 'telefono', FILTER_SANITIZE_STRING);
+    $postal = filter_input(INPUT_GET, 'postal', FILTER_SANITIZE_STRING);
+    $ciudad = filter_input(INPUT_GET, 'ciudad', FILTER_SANITIZE_STRING);
+    $domicilio = filter_input(INPUT_GET, 'domicilio', FILTER_SANITIZE_STRING);
+    $contrasena = filter_input(INPUT_GET, 'contraseña', FILTER_SANITIZE_STRING);
+    echo "<br>";
+
+    $uno ="/^[a-zA-Z\s]*$/";
+    $hola = "hola45";
+    $uno1 = preg_match($uno,$hola);
+    echo $uno1;
+    echo $hola;
+    echo $nombre;
 
 
-$nombre = filter_input(INPUT_GET, 'nombre', FILTER_SANITIZE_STRING);
-$apellido = filter_input(INPUT_GET, 'apellido', FILTER_SANITIZE_STRING);
-$email = filter_input(INPUT_GET, 'email', FILTER_SANITIZE_STRING);
-$telefono = filter_input(INPUT_GET, 'telefono', FILTER_SANITIZE_STRING);
-$postal = filter_input(INPUT_GET, 'postal', FILTER_SANITIZE_STRING);
-$ciudad = filter_input(INPUT_GET, 'ciudad', FILTER_SANITIZE_STRING);
-$domicilio = filter_input(INPUT_GET, 'domicilio', FILTER_SANITIZE_STRING);
-$password = filter_input(INPUT_GET, 'contraseña', FILTER_SANITIZE_STRING);
+    $uno = "//";
+    $dos = "//";
+    $tres = "//";
+    $cuatro = "//";
+    $cinco = "//";
+    $seis = "//";
+    $siete = "//";
+    $ocho = "//";
+
+
+    $re1 = preg_match($uno,$nombre);
+    $re2 = preg_match($dos,$apellido);
+    $re3 = preg_match($tres,$email);
+    $re4 = preg_match($cuatro,$telefono);
+    $re5 = preg_match($cinco,$postal);
+    $re6 = preg_match($seis,$ciudad);
+    $re7 = preg_match($siete,$domicilio);
+    $re8 = preg_match($ocho,$contrasena);
+
 
 
 ?>
 
+
 <!DOCTYPE html>
 <html lang="es">
-
 <head>
     <meta charset="UTF-8">
-    <title>Ejercicio 2</title>
+    <title>Practica 5</title>
     <link rel="stylesheet" href="css/style.css">
-
 </head>
 <?php
     $muestra = [
-            'nombre' => 'Jorge',
-            'apellido' => 'Fuentes',
-            'email' => 'jorgeylucas@gmail.com',
-            'telefono' => '662437243',
-            'postal' => '28400',
-            'ciudad' => 'Collado Villalba',
-            'domicilio' => 'C// Alfonso 13, Nº 151',
-            'contraseña' => 'Aaaaaaaa1#'
+        'nombre' => 'JORGE',
+        'apellido' => 'Fuentes',
+        'email' => 'jorgeylucas@gmail.com',
+        'telefono' => '662437243',
+        'postal' => '28400',
+        'ciudad' => 'Collado Villalba',
+        'domicilio' => 'C// Alfonso 13, Nº 151',
+        'contraseña' => 'Aaaaaaaa1#'
     ]
 ?>
 
@@ -53,64 +79,85 @@ $password = filter_input(INPUT_GET, 'contraseña', FILTER_SANITIZE_STRING);
 <div id="contenedor">
     <div class="header">
         <h1>¡Bienvenido a Banco Santander!</h1>
-        <h2><b>Contraseña aceptada.</b></h2><br /><br />
 
-        <form name="myForm" action="respuestas.php" method="post">
+        <form name="myForm" action="index.php" method="post">
 
             <label for="fnombre">Nombre:</label>
             <input type="text" name="nombre" id="nombre" value="<?php echo $muestra['nombre'] ?>"><br><br>
+            <?php if($re1 == 0){?>
+                    <p class="error">Solo puedes poner letras</p>
+            <?php } ?>
+
 
             <label for="fapellido">Apellidos:</label>
             <input type="text" name="apellido" id="apellido" value="<?php echo $muestra['apellido'] ?>" ><br><br>
+            <?php if($re2 == 0){?>
+                <p class="error">Solo puedes poner letras</p>
+            <?php } ?>
+
 
             <label for="femail">Email:</label>
             <input type="email" name="email" value="<?php echo $muestra['email'] ?>"><br/><br/>
+            <?php if($re3 == 0){?>
+                <p class="error"></p>
+            <?php } ?>
+
 
             <label for="ftelefono">Telefono:</label>
-            <input id="telefono" name="telefono" type="tel" value="<?php echo $muestra['telefono'] ?>"><br><br>
+            <input type="tel" id="telefono" name="telefono" value="<?php echo $muestra['telefono'] ?>"><br><br>
+            <?php if($re4 == 0){?>
+                <p class="error"></p>
+            <?php } ?>
 
             <label for="fpostal">Código Postal:</label>
-            <input id="postal" name="postal" type="text" inputmode="numeric" value="<?php echo $muestra['postal'] ?>"><br><br>
+            <input type="text" id="postal" name="postal" inputmode="numeric" value="<?php echo $muestra['postal'] ?>"><br><br>
+            <?php if($re5 == 0){?>
+                <p class="error"></p>
+            <?php } ?>
 
             <label for="fciudad">Ciudad:</label>
-            <input id="ciudad" name="ciudad" type='country' value="<?php echo $muestra['ciudad'] ?>"><br><br>
+            <input type='country' id="ciudad" name="ciudad" value="<?php echo $muestra['ciudad'] ?>"><br><br>
+            <?php if($re6 == 0){?>
+                <p class="error"></p>
+            <?php } ?>
 
             <label for="fdomicilio">Domicilio:</label>
-            <input id="domicilio" name="domicilio" type='country' value="<?php echo $muestra['domicilio'] ?>"><br><br>
+            <input type='country' id="domicilio" name="domicilio" value="<?php echo $muestra['domicilio'] ?>"><br><br>
+            <?php if($re7 == 0){?>
+                <p class="error"></p>
+            <?php } ?>
 
             <label for="fcontraseña">Contraseña:</label>
-            <input type="text" name="contraseña" id="contraseña"  value="<?php echo $muestra['contraseña'] ?>" ><br><br><br><br>
+            <input type="text" name="contraseña" id="contraseña" value="<?php echo $muestra['contraseña'] ?>" ><br /><br />
+            <?php if($re8 == 0){?>
+                <p class="error"></p>
+            <?php } ?>
+
 
             <select name="transporte">
-
                 <option value="Seleccione ciudad">Seleccione ciudad</option>
 
                 <?php
-                define("fileName", "archivo.txt");
-                global $ciudades;
-                global $opciones;
-                $lineas=file_get_contents(fileName);
-                $ciudades=explode("\n",$lineas);
+                    define("fileName", "archivo.txt");
+                    global $ciudades;
+                    global $opciones;
+                    $lineas=file_get_contents(fileName);
+                    $ciudades=explode("\n",$lineas);
 
-                foreach($ciudades as $ciudad){
-                    $opciones.='<option value="'.$ciudad.'">'.$ciudad.'</option>';
-                }
-                echo $opciones;
-            ?>
+                    foreach($ciudades as $ciudad){
+                        $opciones.='<option value="'.$ciudad.'">'.$ciudad.'</option>';
+                    }
+                    echo $opciones;
+                ?>
 
                 </option>
-
             </select>
-            <br><br><br>
-            <input type="submit" value="Enviar este formulario"/>
 
-            <input type="reset" value="Restablecer"/>
-
+            <input type="button" value="Confirmar" onClick="verificar()">
         </form>
 
     </div>
 </div>
 
 </body>
-
 </html>
