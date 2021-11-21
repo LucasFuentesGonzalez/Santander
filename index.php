68e5c28f101b0f5f0dda9bfa4021e1581991c009
Jorgitor19 <91979954+Jorgitor19@users.noreply.github.com>
2021-11-21 16:54:14 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,74 +1,74 @@
 <?php
 
-    $meses = [];
-    $string = file_get_contents("archivo.txt");
-    $array = explode("\n",$string);
-    foreach ($array as $fila){
-        $item = explode(" ",$fila);
-        $meses[] = [
-            'numero' => $item[0],
-            'nombre' => $item[1]
-        ];
-    }
-
-    $muestra = [
-        'nombre' => 'JORGE',
-        'apellido' => 'Fuentes',
-        'email' => 'jorgeylucas@gmail.com',
-        'telefono' => '662437243',
-        'postal' => '28400',
-        'ciudad' => 'Collado Villalba',
-        'domicilio' => 'C// Alfonso 13, Nº 151',
-        'contraseña' => 'Aaaaaaaa1#',
-        'web' => 'https://jairogarciarincon.com/clase/programacion-en-php/practica-formulario-de-entrada-de-datos',
+$meses = [];
+$string = file_get_contents("archivo.txt");
+$array = explode("\n",$string);
+foreach ($array as $fila){
+    $item = explode(" ",$fila);
+    $meses[] = [
+        'numero' => $item[0],
+        'nombre' => $item[1]
     ];
+}
+
+$muestra = [
+    'nombre' => 'JORGE',
+    'apellido' => 'Fuentes',
+    'email' => 'jorgeylucas@gmail.com',
+    'telefono' => '662437243',
+    'postal' => '28400',
+    'ciudad' => 'Collado Villalba',
+    'domicilio' => 'C// Alfonso 13, Nº 151',
+    'contraseña' => '********',
+    'web' => 'https://jairogarciarincon.com/clase/programacion-en-php/practica-formulario-de-entrada-de-datos',
+];
 
 
-    if(isset($_POST['Confirmar'])) {
-        $nombre = filter_input(INPUT_POST, 'nombre', FILTER_SANITIZE_STRING);
-        $apellido = filter_input(INPUT_POST, 'apellido', FILTER_SANITIZE_STRING);
-        $email = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_STRING);
-        $telefono = filter_input(INPUT_POST, 'telefono', FILTER_SANITIZE_STRING);
-        $postal = filter_input(INPUT_POST, 'postal', FILTER_SANITIZE_STRING);
-        $ciudad = filter_input(INPUT_POST, 'ciudad', FILTER_SANITIZE_STRING);
-        $domicilio = filter_input(INPUT_POST, 'domicilio', FILTER_SANITIZE_STRING);
-        $contrasena = filter_input(INPUT_POST, 'contraseña', FILTER_SANITIZE_STRING);
-        $web = filter_input(INPUT_POST, 'web', FILTER_SANITIZE_STRING);
-    }
+if(isset($_POST['Confirmar'])) {
+    $nombre = filter_input(INPUT_POST, 'nombre', FILTER_SANITIZE_STRING);
+    $apellido = filter_input(INPUT_POST, 'apellido', FILTER_SANITIZE_STRING);
+    $email = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_STRING);
+    $telefono = filter_input(INPUT_POST, 'telefono', FILTER_SANITIZE_STRING);
+    $postal = filter_input(INPUT_POST, 'postal', FILTER_SANITIZE_STRING);
+    $ciudad = filter_input(INPUT_POST, 'ciudad', FILTER_SANITIZE_STRING);
+    $domicilio = filter_input(INPUT_POST, 'domicilio', FILTER_SANITIZE_STRING);
+    $contrasena = filter_input(INPUT_POST, 'contraseña', FILTER_SANITIZE_STRING);
+    $web = filter_input(INPUT_POST, 'web', FILTER_SANITIZE_STRING);
+}
 
-    $re1 = preg_match("/[A-Za-z]/",$nombre);
-    $re2 = preg_match("/[A-Za-z]/",$apellido);
-    $re3 = preg_match("/^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,6}+$/",$email);
-    $re4 = preg_match("/[0-9]{9,9}/",$telefono);
-    $re5 = preg_match("/[0-9]{5,5}/",$postal);
-    $re6 = preg_match("/[A-Za-z]/",$ciudad);
-    $re7 = preg_match("/[A-Za-z]/",$domicilio); //FALTA ESTE
-    $re8 = preg_match("/((?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[^A-Za-z0-9])).{8,16}/",$contrasena);
-    $re9 = preg_match("/[A-Za-z]/",$web); //FALTA ESTE
-
-
-    $error_nombre = " ";
-    $error_apellidos = " ";
-    $error_email = " ";
-    $error_telefono = " ";
-    $error_postal = " ";
-    $error_ciudad = " ";
-    $error_domicilio = " ";
-    $error_contrasena = " ";
-    $error_web = " ";
+$re1 = preg_match("/[A-Za-z]/",$nombre);
+$re2 = preg_match("/[A-Za-z]/",$apellido);
+$re3 = preg_match("/^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,6}+$/",$email);
+$re4 = preg_match("/[0-9]{9,9}/",$telefono);
+$re5 = preg_match("/[0-9]{5,5}/",$postal);
+$re6 = preg_match("/[A-Za-z]/",$ciudad);
+$re7 = preg_match("/[A-Za-z]/",$domicilio); //FALTA ESTE
+$re8 = preg_match("/((?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[^A-Za-z0-9])).{8,16}/",$contrasena);
+$re9 = preg_match("/[A-Za-z]/",$web); //FALTA ESTE
 
 
-    if(isset($_POST['Confirmar'])) {
-        if ($re1 == 0){$error_nombre = "Solo puedes poner letras<br>";}
-        if ($re2 == 0){$error_apellidos = "Solo puedes poner letras<br>";}
-        if ($re3 == 0){$error_email = "Introduce un email válido<br>";}
-        if ($re4 == 0){$error_telefono = "Solo puedes poner numeros<br>";}
-        if ($re5 == 0){$error_postal = "Solo se permiten 5 numeros en estes campo<br>";}
-        if ($re6 == 0){$error_ciudad = "Solo puedes poner letras<br>";}
-        if ($re7 == 0){$error_domicilio = "Solo puedes poner letras<br>";}
-        if ($re8 == 0){$error_contrasena = "Debes poner una contraseña con una mayuscula, una minuscula, un numero y un caracter especial<br>";}
-        if ($re9 == 0){$error_web = "Introduce un link válido<br>";}
-    }
+$error_nombre = " ";
+$error_apellidos = " ";
+$error_email = " ";
+$error_telefono = " ";
+$error_postal = " ";
+$error_ciudad = " ";
+$error_domicilio = " ";
+$error_contrasena = " ";
+$error_web = " ";
+
+
+if(isset($_POST['Confirmar'])) {
+    if ($re1 == 0){$error_nombre = "Solo puedes poner letras<br>";}
+    if ($re2 == 0){$error_apellidos = "Solo puedes poner letras<br>";}
+    if ($re3 == 0){$error_email = "Introduce un email válido<br>";}
+    if ($re4 == 0){$error_telefono = "Solo puedes poner numeros<br>";}
+    if ($re5 == 0){$error_postal = "Solo se permiten 5 numeros en estes campo<br>";}
+    if ($re6 == 0){$error_ciudad = "Solo puedes poner letras<br>";}
+    if ($re7 == 0){$error_domicilio = "Solo puedes poner letras<br>";}
+    if ($re8 == 0){$error_contrasena = "Debes poner una contraseña con una mayuscula, una minuscula, un numero y un caracter especial<br>";}
+    if ($re9 == 0){$error_web = "Introduce un link válido<br>";}
+}
 
 ?>
 
@@ -81,6 +81,8 @@
     <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
+
+<img class="santander" src="https://i.imgur.com/R0A5EZP.png">
 
 <div id="contenedor">
     <div class="header">
@@ -135,18 +137,18 @@
 
             <select name="provincia">
                 <option value="Seleccione provincia">Seleccione provincia</option>
-                    <?php
-                        define("fileName", "archivo.txt");
-                        global $ciudades;
-                        global $opciones;
-                        $lineas=file_get_contents(fileName);
-                        $ciudades=explode("\n",$lineas);
+                <?php
+                define("fileName", "archivo.txt");
+                global $ciudades;
+                global $opciones;
+                $lineas=file_get_contents(fileName);
+                $ciudades=explode("\n",$lineas);
 
-                        foreach($ciudades as $ciudad){
-                            $opciones.='<option value="'.$ciudad.'">'.$ciudad.'</option>';
-                        }
-                        echo $opciones;
-                    ?>
+                foreach($ciudades as $ciudad){
+                    $opciones.='<option value="'.$ciudad.'">'.$ciudad.'</option>';
+                }
+                echo $opciones;
+                ?>
                 </option>
 
             </select><br><br>
