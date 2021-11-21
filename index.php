fd3bd918f40b3c8f93ab3e72ac0e03c1992fc29c
LucasFuentesGonzalez <91980274+LucasFuentesGonzalez@users.noreply.github.com>
2021-11-21 01:17:37 +0100

    Update index.php

--- index.php
+++ index.php
@@ -33,13 +33,13 @@
         $ciudad = filter_input(INPUT_POST, 'ciudad', FILTER_SANITIZE_STRING);
         $domicilio = filter_input(INPUT_POST, 'domicilio', FILTER_SANITIZE_STRING);
         $contrasena = filter_input(INPUT_POST, 'contraseña', FILTER_SANITIZE_STRING);
-        $web = filter_input(INPUT_POST, '', FILTER_SANITIZE_STRING);
+        $web = filter_input(INPUT_POST, 'web', FILTER_SANITIZE_STRING);
     }
 
 
     $uno = "/^[a-zA-Z\s]*$/"; //(letras mayus y minus)     /^[a-zA-Z\s]*$/
     $dos = "/^[a-zA-Z\s]*$/"; //(letras mayus y minus)      /^[a-zA-Z\s]*$/
-    $tres = "//"; // (letras mayus y minus y numeros) + @ + (letras solo minus) + . +(letras solo minus)                   /\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,6}\b/
+    $tres = "//"; // (letras mayus y minus y numeros) + @ + (letras solo minus) + . +(letras solo minus) /\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,6}\b/
     $cuatro = "//"; //(9 numeros del 0 al 9)
     $cinco = "//"; //([CODIGO DEL C.P] + 5 NUMEROS DEL 0 AL 9)
     $seis = "/^[a-zA-Z\s]*$/"; //(Letras mayus y minus)
@@ -55,9 +55,18 @@
     $re5 = preg_match($cinco,$postal);
     $re6 = preg_match('`[A-Za-z]`',$ciudad);
     $re7 = preg_match($siete,$domicilio);
-    $re8 = preg_match($ocho,$contrasena);
+    $re8 = preg_match("/((?=.?[A-Z])(?=.?[a-z])(?=.?[0-9])(?=.?[^A-Za-z0-9])).{8,16}/",$contrasena);
     $re9 = preg_match($nueve,$web);
 
+    $error_nombre = " ";
+    $error_apellidos = " ";
+    $error_email = " ";
+    $error_telefono = " ";
+    $error_postal = " ";
+    $error_ciudad = " ";
+    $error_domicilio = " ";
+    $error_contrasena = " ";
+    $error_web = " ";
 
     if ($re1 == 0){$error_nombre = "Solo puedes poner letras";}
     if ($re2 == 0){$error_apellidos = "Solo puedes poner letras";}
@@ -85,64 +94,71 @@
     <div class="header">
         <h1>¡Bienvenido a Banco Santander!</h1>
 
-        <form name="myForm" action="index.php" method="post">
+        <form action="index.php" method="post">
 
-            <label for="fnombre">Nombre:</label>
+            <label for="nombre">Nombre:</label>
             <input type="text" id="nombre" name="nombre" value="<?php echo $muestra['nombre'] ?>"><br><br>
             <?php echo $error_nombre ?>
 
 
-            <label for="fapellido">Apellidos:</label>
+            <label for="apellido">Apellidos:</label>
             <input type="text" id="apellido" name="apellido" value="<?php echo $muestra['apellido'] ?>"><br><br>
             <?php echo $error_apellidos ?>
 
 
-            <label for="femail">Email:</label>
+            <label for="email">Email:</label>
             <input type="email" id="email" name="email" value="<?php echo $muestra['email'] ?>"><br><br>
             <?php echo $error_email ?>
 
 
-            <label for="ftelefono">Telefono:</label>
+            <label for="telefono">Telefono:</label>
             <input type="tel" id="telefono" name="telefono" value="<?php echo $muestra['telefono'] ?>"><br><br>
             <?php echo $error_telefono ?>
 
-            <label for="fpostal">Código Postal:</label>
+
+            <label for="postal">Código Postal:</label>
             <input type="text" id="postal" name="postal" value="<?php echo $muestra['postal'] ?>"><br><br>
             <?php echo $error_postal ?>
 
-            <label for="fciudad">Ciudad:</label>
+
+            <label for="ciudad">Ciudad:</label>
             <input type='text' id="ciudad" name="ciudad" value="<?php echo $muestra['ciudad'] ?>"><br><br>
             <?php echo $error_ciudad ?>
 
-            <label for="fdomicilio">Domicilio:</label>
+
+            <label for="domicilio">Domicilio:</label>
             <input type='text' id="domicilio" name="domicilio" value="<?php echo $muestra['domicilio'] ?>"><br><br>
             <?php echo $error_domicilio ?>
 
-            <label for="fcontraseña">Contraseña:</label>
+
+            <label for="contraseña">Contraseña:</label>
             <input type="text" id="contraseña" name="contraseña" value="<?php echo $muestra['contraseña'] ?>"><br><br>
             <?php echo $error_contrasena ?>
 
-            <label for="fweb">Web:</label>
+
+            <label for="web">Web:</label>
             <input type="url" id="web" name="web" value="<?php echo $muestra['web'] ?>"><br><br>
             <?php echo $error_web ?>
 
+
             <select name="provincia">
                 <option value="Seleccione provincia">Seleccione provincia</option>
-                <?php
-                    define("fileName", "archivo.txt");
-                    global $ciudades;
-                    global $opciones;
-                    $lineas=file_get_contents(fileName);
-                    $ciudades=explode("\n",$lineas);
+                    <?php
+                        define("fileName", "archivo.txt");
+                        global $ciudades;
+                        global $opciones;
+                        $lineas=file_get_contents(fileName);
+                        $ciudades=explode("\n",$lineas);
 
-                    foreach($ciudades as $ciudad){
-                        $opciones.='<option value="'.$ciudad.'">'.$ciudad.'</option>';
-                    }
-                    echo $opciones;
-                ?>
+                        foreach($ciudades as $ciudad){
+                            $opciones.='<option value="'.$ciudad.'">'.$ciudad.'</option>';
+                        }
+                        echo $opciones;
+                    ?>
                 </option>
-            </select>
-                <br><br>
+
+            </select><br><br>
+
             <input type="submit" name="Confirmar" value="Confirmar">
         </form>
     </div>
@@ -150,3 +166,6 @@
 
 </body>
 </html>
+
+
+
